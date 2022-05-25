6f1c13a37a0ba2c03fd3c6f9d8fced40512473ff
Damir <damir@MacBook-Pro-Margarita.local>
2022-05-25 17:29:08 +0600

    updated pizzaBlock

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Categories from './components/Categories'
 import Sort from './components/Sort'
 import PizzaBlock from './components/PizzaBlock'
 
+import pizzas from './assets/pizzas.json'
+
 function App() {
   return (
     <div className='wrapper'>
@@ -18,9 +20,9 @@ function App() {
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-            <PizzaBlock title='Мексиканская' price='500' />
-            <PizzaBlock title='Итальянская' price='550' />
-            <PizzaBlock />
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,25 @@
+import React, { useState } from 'react'
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index)
+  }
+
   return (
     <div className='categories'>
       <ul>
-        <li className='active'>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {/* <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? 'active' : ''}>
+          Все
+        </li> */}
+        {categories.map((it, i) => (
+          <li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''}>
+            {it}
+          </li>
+        ))}
       </ul>
     </div>
   )
